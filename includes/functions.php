23e55eab9e8b55e0e8bd1ca371c1fe2380b6288f
Your Name <your_email@example.com>
2026-09-25 07:41:27 +0100

    Fix Baku hero image 404

--- includes/functions.php
+++ includes/functions.php
@@ -497,7 +497,7 @@ function getRaceHeroImage($country) {
         'Belgium' => 'https://img.redbull.com/images/c_crop,x_0,y_6,h_667,w_1000/c_fill,w_1850,h_1256/q_auto:low,f_jpg/redbullcom/2020/4/24/fkuxeu9tjzfdbmmuwzof/red-bull-racing-dutch-windmills',
         'Netherlands' => 'https://images.unsplash.com/photo-1512470876302-972faa2aa9a4?q=80&w=2070&auto=format&fit=crop',
         'Italy' => 'https://images.unsplash.com/photo-1529260830199-42c24126f198?q=80&w=2070&auto=format&fit=crop',
-        'Azerbaijan' => 'https://images.unsplash.com/photo-1549420070-5b1b4b2cb82b?q=80&w=2070&auto=format&fit=crop',
+        'Azerbaijan' => 'https://images.unsplash.com/photo-1744701821206-15eef2edcb1e?q=80&w=2070&auto=format&fit=crop',
         'Singapore' => 'https://images.unsplash.com/photo-1525625230556-3a56cc544253?q=80&w=2070&auto=format&fit=crop',
         'Usa' => 'https://images.unsplash.com/photo-1485738422979-f5c462d49f74?q=80&w=2070&auto=format&fit=crop',
         'USA' => 'https://images.unsplash.com/photo-1485738422979-f5c462d49f74?q=80&w=2070&auto=format&fit=crop',
